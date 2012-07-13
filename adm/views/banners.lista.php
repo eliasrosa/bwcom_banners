60993f25fc7b8179c8d8a2aebbdf11dccef2f731
Elias da Rosa <elias@eliasdarosa.com.br>
2012-07-13 00:13:22 -0300

    Atualizado e removido alguns arquivos

--- adm/views/banners.lista.php
+++ adm/views/banners.lista.php
@@ -1,4 +1,5 @@
 <?
+
 defined('BW') or die("Acesso negado!");
 
 echo bwAdm::createHtmlSubMenu(1);
@@ -6,36 +7,37 @@ echo bwButton::redirect('Criar novo banner', 'adm.php?com=banners&view=cadastro'
 
 class bwGridBannersLista extends bwGrid
 {
+
     function __construct()
     {
         parent::__construct(
             Doctrine_Query::create()
                 ->from('Banner')
         );
-        
+
         $this->addCol('ID', 'id', 'tac', 30);
         $this->addCol('Nome', 'nome');
         $this->addCol('Observações', 'observacoes', NULL, '40%');
         $this->addCol('Status', 'status', 'tac', 30);
-        
+
         $this->show();
     }
-    
+
     function col0($i)
     {
-        return $i->id;
+        return sprintf('<a href="%s">%s</a>', $i->getUrl('/adm/banners/cadastro'), $i->id);
     }
-    
+
     function col1($i)
     {
-        return '<a href="' . bwRouter::_('adm.php?com=banners&view=cadastro&id=' . $i->id) . '">'.$i->nome.'</a>';
+        return $i->nome;
     }
-    
+
     function col2($i)
     {
         return $i->observacoes;
     }
-    
+
     function col3($i)
     {
         return bwAdm::getImgStatus($i->status);
@@ -46,5 +48,5 @@ class bwGridBannersLista extends bwGrid
 new bwGridBannersLista();
 ?>
 
-    
+
 
