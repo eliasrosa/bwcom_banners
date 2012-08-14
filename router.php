0530951a71141761782da43ef5f1fd522a201ecb
Elias da Rosa <elias@eliasdarosa.com.br>
2012-08-14 11:14:29 -0300

    Alterado as rotas

--- router.php
+++ router.php
@@ -1,10 +1,12 @@
 <?
+
 defined('BW') or die("Acesso negado!");
 
 // ADM
 bwRouter::addUrl('/banners');
-bwRouter::addUrl('/banners/task', array(), 'task');
+bwRouter::addUrl('/banners/task', array('type' => 'task'));
 bwRouter::addUrl('/banners/lista');
-bwRouter::addUrl('/banners/cadastro/:id', array('id'));
+bwRouter::addUrl('/banners/cadastro/:id', array('fields' => array('id')));
 bwRouter::addUrl('/banners/slides/lista');
-bwRouter::addUrl('/banners/slides/cadastro/:id', array('id'));
+bwRouter::addUrl('/banners/slides/cadastro/:id', array('fields' => array('id')));
+
